b5f39f658a624660a4958057ecb9e8e98cee69d5
Sangjun, Park <libedi@gmail.com>
2018-08-02 23:43:38 +0900

    change filename

--- spring-web-sample/src/main/java/kr/co/tworld/shop/SpringWebSampleApplication.java
+++ spring-web-sample/src/main/java/kr/co/tworld/shop/SpringWebSampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwddSampleApplication {
+public class SpringWebSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwddSampleApplication.class, args);
+		SpringApplication.run(SpringWebSampleApplication.class, args);
 	}
 }
--- spring-web-sample/src/test/java/kr/co/tworld/shop/SpringWebSampleApplicationTests.java
+++ spring-web-sample/src/test/java/kr/co/tworld/shop/SpringWebSampleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TwddSampleApplicationTests {
+public class SpringWebSampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
