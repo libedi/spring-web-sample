45ad38ce90a58ade1c7ccd96c7630d4adad2c9e4
Sang jun, Park <Sang jun, Park@TD3P127593.SKCC.NET>
2018-08-03 17:30:03 +0900

    add @Requestbody annotation

--- spring-web-sample/src/main/java/kr/co/tworld/shop/sample/controller/SampleApiController.java
+++ spring-web-sample/src/main/java/kr/co/tworld/shop/sample/controller/SampleApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,7 +60,7 @@ public class SampleApiController {
 	 */
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
-	public void createCustomer(@Validated(Create.class) final Sample sample) {
+	public void createCustomer(@RequestBody @Validated(Create.class) final Sample sample) {
 		this.sampleService.createCustomer(sample);
 	}
 
@@ -69,7 +70,7 @@ public class SampleApiController {
 	 */
 	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH}, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public void updateCustomer(@Validated(Update.class) final Sample sample) {
+	public void updateCustomer(@RequestBody @Validated(Update.class) final Sample sample) {
 		this.sampleService.updateCustomer(sample);
 	}
 
